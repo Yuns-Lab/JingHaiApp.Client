898b97d1a276161597156e34515ce9d1a9a15417
JingHai_Lingyun <m56042764@163.com>
2023-10-20 14:30:50 +0800

    :camera_flash: :construction: 入会预审界面新加卡片

--- static/js/pages/joinexam.js
+++ static/js/pages/joinexam.js
@@ -7,8 +7,14 @@ function page_joinexam__editElements(){
         const offsetleft = document.getElementById("2-text-welcome-name").offsetWidth + 152;
         document.getElementById("2-text-joinstatus").style.left = offsetleft + "px";
         document.getElementById("2-text-joinstatus").style.display = "block";
+        if (localStorage.getItem("premission") == "1"){
+            document.getElementById("2-text-info").innerHTML = "请选择预审核方向"
+            document.getElementById("2-text-info").style.left = "270px";
+            document.getElementById("2-text-info").style.display = "block";
+        }
         if (localStorage.getItem("premission") == "4" || localStorage.getItem("premission") == "5"){
-            document.getElementById("2-text-youcanedit").style.display = "block";
+            document.getElementById("2-text-info").innerHTML = "你有权限查看/批准/拒绝入会预审列表"
+            document.getElementById("2-text-info").style.display = "block";
         }
     }
 }
